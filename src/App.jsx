efb9d8aae6e42276376561eeb41c1ca645cdfdb5
rigogarcia7060@gmail.com <rigogarcia7060@gmail.com>
2025-02-05 12:58:51 -0800

    Added props feature to App.jsx and Card.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import Card from "./Card.jsx";
 function App() {
   return(
     <>
-      <Card/>
-      <Card/>
+      <Card name="Rigo Garcia" bio="Computer Science Student at Santa Clara University!" gradYr={2026} isLEAD={true}/>
+      <Card name= "Steven Chang" bio="Computer Engineering Student at Santa Clara University" gradYr={2025}/>
+      <Card />
     </>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,29 @@
 import profilePic from './assets/tempPFP.png';
+import PropTypes from 'prop-types';
 
-function Card(){
+function Card(props){
     return(
         <div className="card">
             <img className="card-img" src={profilePic} alt="random pfp"></img>
-            <h2 className="card-title">Rigo Garcia</h2>
-            <p>Hi! I am a 3rd year Computer Science student at
-                Santa Clara University!
-            </p>
-
+            <h2 className="card-title">Name: {props.name}</h2>
+            <p>{props.bio}</p>
+            <p>Graduation Year: {props.gradYr}, Lead Scholar: {props.isLEAD ? "Yes" : "No"}</p>
         </div>
     );
+}
+
+Card.propTypes = {
+    name: PropTypes.string,
+    bio: PropTypes.string,
+    gradYr: PropTypes.number,
+    isLEAD: PropTypes.bool,
+}
+
+Card.defaultProps = {
+    name: "John Smith",
+    bio: "Student at Santa Clara University",
+    gradYr: -1,
+    isLEAD: false,
 }
 
 export default Card
